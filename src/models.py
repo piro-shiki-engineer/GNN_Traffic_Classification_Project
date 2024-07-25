de020f1948522e697465185cde5ad097d2ab20eb
hiroya <hiroya3508@gmail.com>
2024-07-25 22:33:07 +0900

    モデルの修正

--- src/models.py
+++ src/models.py
@@ -1,8 +1,8 @@
 import torch
-from torch.nn import Linear
 import torch.nn.functional as F
+from torch.nn import Linear
+# noinspection PyProtectedMember
 from torch_geometric.nn import GCNConv
-from torch_geometric.data import DataLoader
 from torch_geometric.nn import global_mean_pool
 
 
